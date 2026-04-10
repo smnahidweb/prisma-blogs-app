4031488392800e172f27dec76fa41e8ca034359b
unknown <smnahidh040@gmail.com>
2026-04-11 02:57:05 +0600

    fixed the better auth error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,13 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { Prisma } from "../../generated/prisma/client";
-// If your Prisma file is located elsewhere, you can change the path
 
+import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-    database: prismaAdapter(Prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    database: prismaAdapter(prisma, { // এখানে তৈরি করা 'prisma' ইন্সট্যান্সটি দিন
+        provider: "postgresql",
     }),
     emailAndPassword: { 
-    enabled: true, 
-  }, 
+        enabled: true, 
+    }, 
 });
