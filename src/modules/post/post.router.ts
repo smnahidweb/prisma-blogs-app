01de905565ab829b071dbd033fdef698a8bd6fd8
smnahidweb <smnahidh040@gmail.com>
2026-04-13 00:47:56 +0600

    updted the post router

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -3,6 +3,26 @@ import { postController } from "./post.controller";
 import {auth as betterAuth} from "../../lib/auth"
 const router = express.Router();
 
+export enum UserRole {
+    USER = "USER",
+    ADMIN = "ADMIN" 
+}
+
+declare global {
+   namespace Express {
+    interface Request {
+        user?:{
+            id:string;
+            email:string;
+            name:string;
+            role: string;
+            emailVerified:boolean
+
+        }
+    }
+   }
+}
+
 const auth = (...roles:any)=>{
     return async(req:Request,res:Response,next:NextFunction)=>{
       const session = await betterAuth.api.getSession({
@@ -22,6 +42,15 @@ const auth = (...roles:any)=>{
             success:false,
             message:"Your email isnt verified . Please verified the email first"
         })
+      }
+
+      req.user = {
+         id:session.user.id,
+         email: session.user.email,
+         name: session.user.name,
+         role: session.user.role as string,
+         emailVerified:session.user.emailVerified,
+
       }
     
     
