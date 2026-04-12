f3bdeb3dc9701eb14ab92a1622ad8ee717d7a0b8
unknown <smnahidh040@gmail.com>
2026-04-12 07:00:23 +0600

    add emaail verification

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,16 @@ import express from "express";
 import { postRouter } from "./modules/post/post.router";
 import { auth } from "./lib/auth";
 import { toNodeHandler } from "better-auth/node";
-
+import cors from "cors";
 const app = express();
 app.use(express.json())
+
+app.use(cors({
+        origin: process.env.APP_URL || "http://localhost:4000",
+        credentials: true,
+}))
+
+
 app.use("/api/auth", (req, res) => {
     // এটি /api/auth দিয়ে শুরু হওয়া সব রিকোয়েস্ট হ্যান্ডেল করবে
     return toNodeHandler(auth)(req, res);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,71 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
 import { prisma } from "./prisma";
 
+import nodemailer from "nodemailer";
+
+
+
+// nodemailer 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  auth: {
+    user: process.env.APP_USER,
+    pass: process.env.APP_PASS,
+  },
+});
+
+
+
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, { // এখানে তৈরি করা 'prisma' ইন্সট্যান্সটি দিন
+    database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
-    emailAndPassword: { 
-        enabled: true, 
-    }, 
+
+    trustedOrigins : [process.env.APP_URL || "http://localhost:4000"],
+
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                defaultValue: "USER",
+                required: false,
+            },
+            phone: {
+                type: "string",
+                required: false,
+            },
+            status: {
+                type: "string",
+                defaultValue: "ACTIVE",
+                required: false,
+            }
+        }
+    },
+
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: false,
+        requireEmailVerification:true
+    },
+
+   emailVerification:{
+     sendVerificationEmail: async ( { user, url, token }, request) => {
+     console.log({url, token, user})
+     const verificationLink = `${process.env.APP_URL}/verify-email?token=${token}`;
+
+    const info = await transporter.sendMail({
+    from: '"Prisma Blog Team" <prisma@blog.com>', // sender address
+    to: "smnahidhasan788@gmail.com", // list of recipients
+    subject: "Hello", // subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // HTML body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+    },
+   }
+
 });
